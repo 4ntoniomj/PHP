7834fd73b69384e0fd669b1453961e9d9216ba4e
miguel <ma.molinamartinezz@gmail.com>
2025-11-28 00:42:27 +0100

    CSRF para nuestro formulario

---
+++ phpwww-data/app/csrf.php
@@ -0,0 +1,24 @@
+<?php
+// =====  Protección CSRF para formularios ====== //
+ 
+// Nos genera y almacena el token CSRF
+
+function generate_csrf_token() {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+ // Se verifica que el token anteriormente generado llega con la peticion del formulario 
+
+function verify_csrf_token($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+// Se elimina el toquen creado en la sesion
+ 
+function clear_csrf_token() {
+    unset($_SESSION['csrf_token']);
+}
+?>
