4fb4331bb0f2ab974e9a3447a19dee401067493a
Antonio Madrid <mj.antonio.madrid@gmail.com>
2025-11-27 22:49:46 +0100

    Estructura de directorios

---
+++ iniciar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Fecha: lun 20 oct 2025 10:28:14 CEST
+# Descripción: Iniciar docker
+# Versión: 1.0
+
+# Color palette
+greenColor="\e[0;32m[1m"
+endColor="\033[0m[0m"
+redColor="\e[0;31m[1m"
+blueColor="\e[0;34m[1m"
+yellowColor="\e[0;33m[1m"
+purpleColor="\e[0;35m[1m"
+turquoiseColor="\e[0;36m[1m"
+grayColor="\e[0;37m[1m"
+
+docker compose up -d
+
+echo -e "Para entrar a la base de datos ejecute ${yellowColor}docker exec -it bsdKeePass mysql -u root -p${endColor}\nLa contraseña por defecto es ${redColor}123456789${endColor}"
+
+sudo chown $USER:$USER -R data
