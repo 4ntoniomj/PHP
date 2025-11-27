0da74d706f48206eff5dd48170e84e1c17ab81bd
Jota <jotajotamuto@gmail.com>
2025-11-28 00:44:59 +0100

    PDO con funcion

--- phpwww-data/app/pdo.php
+++ phpwww-data/app/pdo.php
@@ -1,23 +1,27 @@
 <?php
-/* =====  CONFIGURAR CONEXIÓN PDO ====== */
-$host = 'bsd';
-$dbname = 'GDI';
-$user = 'root';
-$pass = '123456789';
+function getPDO() {
+
+    /* =====  CONFIGURAR CONEXIÓN PDO ====== */
+    $host = 'bsd';
+    $dbname = 'GDI';
+    $user = 'root';
+    $pass = '123456789';
 
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4", // utf8 para no tener problemas con acentos y signos
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // si algo falla en SQL, lanza excepcion
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //nos devuelve arrays con clave->valor
-            PDO::ATTR_EMULATE_PREPARES   => false //usa consultas SQL parametrizadas, para prevenir inyeccion de código SQL
-        ]
-    );
-} catch (PDOException $e) {
-    die("Error de conexión BD: " . $e->getMessage()); //Captura errores y los guarda en $e
+    try {
+        $pdo = new PDO(
+            "mysql:host=$host;dbname=$dbname;charset=utf8mb4", // utf8 para no tener problemas con acentos y signos
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // si algo falla en SQL, lanza excepcion
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //nos devuelve arrays con clave->valor
+                PDO::ATTR_EMULATE_PREPARES   => false //usa consultas SQL parametrizadas, para prevenir inyeccion de código SQL
+            ]
+        );
+        return $pdo; 
+    } catch (PDOException $e) {
+        die("Error de conexión BD: " . $e->getMessage()); //Captura errores y los guarda en $e
+    }
 }
 ?>
