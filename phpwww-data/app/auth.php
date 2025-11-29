fb8e53ce92281ecffb032faf72f46593302f8607
Jota <jotajotamuto@gmail.com>
2025-11-29 19:41:57 +0100

    Arreglada redirección

--- phpwww-data/app/auth.php
+++ phpwww-data/app/auth.php
@@ -5,7 +5,7 @@ require_once '../app/pdo.php';
 function require_login() {
     if (!isset($_SESSION['user_id'])) {
         $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
-        header('Location: /GDI/public/login.php');
+        header('Location: /public/login.php');
         exit;
     }
 }
